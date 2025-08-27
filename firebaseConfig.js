c86d8acaa851a0109bfb86453610ab6f8ffd77b8
cabalida <gilcabalida570@gmail.com>
2025-08-27 09:48:24 +0800

    firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+inimport { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBR-TbAaehihZcZZramUG91RqJFWJXFII0",
+  authDomain: "jkl-goals.firebaseapp.com",
+  projectId: "jkl-goals",
+  storageBucket: "jkl-goals.firebasestorage.app",
+  messagingSenderId: "300933876692",
+  appId: "1:300933876692:web:6b047e1a43cb0582936441"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
